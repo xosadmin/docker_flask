66d32d78dabdeb5c64afbef107e6537604284bf0
X-osadmin <9164356+xosadmin@users.noreply.github.com>
2024-08-14 21:30:04 +0800

    Update start.sh to fix start error

--- start.sh
+++ start.sh
@@ -5,5 +5,7 @@ if [ ! -f "/app/requirements.txt" ]; then
     exit 1
 fi
 
-pip3 install --no-cache-dir -r /app/requirements.txt
-exec gunicorn --workers 4 --bind 0.0.0.0:8000 app:app 2>&1 | tee -a /var/log/gunicorn.log
+pip3 install --no-cache-dir flask --break-system-packages
+pip3 install --no-cache-dir -r /app/requirements.txt --break-system-packages
+cd /app
+flask run --host=0.0.0.0
