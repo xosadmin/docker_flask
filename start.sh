b777debaa8f3f19a2c03967303ccda1a17787c0d
X-osadmin <9164356+xosadmin@users.noreply.github.com>
2024-08-16 14:39:31 +0800

    Add experimental support of uWSGI

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ if [ ! -f "/app/requirements.txt" ]; then
     exit 1
 fi
 
-pip3 install --no-cache-dir flask --break-system-packages
+pip3 install --no-cache-dir flask uwsgi --break-system-packages
 pip3 install --no-cache-dir -r /app/requirements.txt --break-system-packages
 cd /app
-flask run --host=0.0.0.0
+uwsgi --http :5000 --wsgi-file app.py --callable app
