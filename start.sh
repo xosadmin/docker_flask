9ee94e91aea5e4b4baab181151bca0ffc6bd31d7
X-osadmin <9164356+xosadmin@users.noreply.github.com>
2024-08-16 14:42:24 +0800

    Add support of uwsgi custom configuration

--- start.sh
+++ start.sh
@@ -8,4 +8,10 @@ fi
 pip3 install --no-cache-dir flask uwsgi --break-system-packages
 pip3 install --no-cache-dir -r /app/requirements.txt --break-system-packages
 cd /app
-uwsgi --http :5000 --wsgi-file app.py --callable app
+if [ ! -f "/app/uwsgi.ini" ]; then
+    echo "No uWSGI configuration found. Using default parameters."
+    uwsgi --http :5000 --wsgi-file app.py --callable app
+else
+    echo "Find uWSGI configuration file. Launching uWSGI based on configuration file..."
+    uwsgi --ini uwsgi.ini
+fi
